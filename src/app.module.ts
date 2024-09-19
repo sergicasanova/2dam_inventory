c39fb4c1274438711c8aea229612542ee4088e01
Raul <rauliker04@gmail.com.com>
2024-09-19 16:54:00 +0200

    act

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { ClassroomModule } from './classroom/classroom.module';
 import { InventariTypeModule } from './inventari_type/inventari_type.module';
 import { IssuesModule } from './issues/issues.module';
+import { InventariModule } from './inventari/inventari.module';
+
 
 @Module({
-  imports: [ClassroomModule, InventariTypeModule, IssuesModule],
+  imports: [ClassroomModule, InventariTypeModule, IssuesModule, InventariModule],
   controllers: [AppController],
   providers: [AppService],
 })
