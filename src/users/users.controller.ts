a76da8353d9366b28a712c1985086323981a1765
Alonso <alonsoferrborr@gmail.com>
2024-09-25 15:15:42 +0200

    cambios Users Alonso Ferrandiz

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,43 @@
-import { Body, Controller , Delete, Get , Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { query } from 'express';
-@Controller('users')
+import { error } from 'console';
+@Controller('Status')
 export class UsersController {
-    private  UsersService: UsersService;
-    constructor(UsersService:UsersService){
+    private UsersService: UsersService;
+    constructor(UsersService: UsersService) {
         this.UsersService = UsersService;
     };
     @Get()
-    getAllUser(@Query() query){
-        console.log(query)
-        return this.UsersService.getAllUser();
+    getAllUser(@Query() query) {
+        try {
+            return this.UsersService.getAllUser();
+        } catch (err) {
+            throw new HttpException({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: err,
+            }, HttpStatus.INTERNAL_SERVER_ERROR, {
+                cause: err
+            });
+        }
     }
     @Get(':id')
     getUser(@Param('id') id: string) {
         return this.UsersService.getUser(parseInt(id));
     }
     @Post()
-    createUser(@Body()User){
-        return this.UsersService.createUser(User);
-    }   
+    createUser(@Body() Status) {
+        return this.UsersService.createUser(Status);
+    }
     @Put(':id')
-    UpdateUser(@Param('id') id:string, @Body()User){
+    UpdateUser(@Param('id') id: string, @Body() Status) {
         return this.UsersService.updateUser({
-        ...User,
-        id: parseInt(id)
+            ...Status,
+            id_status: parseInt(id)
         });
     }
     @Delete(':id')
-    deleteTask (@Param('id') id:string){
+    deleteUser(@Param('id') id: string) {
         return this.UsersService.deleteUser(parseInt(id));
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,62 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+const fs = require('node:fs');
+const filepath = path.join(path.resolve(__dirname, '..'),'/data/inventory_users.json');
+ var path = require('path');
+ const UsersData = JSON.parse(fs.readFileSync(filepath, 'utf8'));  
+function SaveData() {
+    fs.writeFileSync(filepath, JSON.stringify(UsersData));
+};
 
 @Injectable()
 export class UsersService {
     getAllUser() {
-        return { message: 'Todos los usuarios han sido recuperados satisfactoriamente' };
-    }
+            return UsersData ;
+}
 
-    createUser(user: any) {
+    createUser(Users: any) {
+        UsersData.push({
+            id_status: UsersData.lenght[UsersData.lenght-1].id_User,
+            ...Users
+        })
         return { message: 'Usuario creado satisfactoriamente' };
     }
 
     getUser(id: number) {
-        return { message: `Usuario con id ${id} recuperado satisfactoriamente` };
+        var i = 0;
+        while(i < UsersData.lenght && UsersData[i].id_User!=id){
+            i++;
+        }
+        if(UsersData[i]){
+        return  UsersData[i];
+        }
+        else{
+            throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+        }
     }
 
-    updateUser(userUpdated: any) {
-        return { message: `Usuario con id ${userUpdated.id} actualizado satisfactoriamente` };
+    updateUser(UsersUpdated: any) {  
+        var i = 0;
+        while(i < UsersData.lenght && UsersData[i].id_User!=UsersUpdated.id_User){
+            i++;
+        }
+        if(UsersData[i]){
+            UsersData[i] = UsersUpdated;
+        return  UsersData[i];
+        }
+        else{
+            throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+        }
+        return { message: `Estado con id ${UsersUpdated.id} actualizado satisfactoriamente` };
     }
 
     deleteUser(id: number) {
-        return { message: `Usuario con id ${id} eliminado satisfactoriamente` };
+        var i = 0;
+        while (i < UsersData.length && UsersData[i].id_User != id ) {
+            i++;
+        }
+        if (UsersData[i]){
+            return UsersData.splice(i,1);
+        }else
+            throw new HttpException('Not found', HttpStatus.NOT_FOUND);
     }
 }
