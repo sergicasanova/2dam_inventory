c12f14a16991fb47443aedd33b3be1ccacca83a4
Alonso <alonsoferrborr@gmail.com>
2024-10-16 19:14:21 +0200

    cambios relaciones Base de datos Inv a InvType Y test

--- src/inventari_type/inventari_type.service.spec.ts
+++ src/inventari_type/inventari_type.service.spec.ts
@@ -22,29 +22,77 @@ interface InventariType {
   description: string;
 }
 
-async function fetchInventariTypes(): Promise<void> {
-  const headersList: HeadersInit = {
-    Accept: '*/*',
-    'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
-  };
+const headersList: HeadersInit = {
+  Accept: '*/*',
+  'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+  'Content-Type': 'application/json',
+};
 
+async function fetchInventariTypes(): Promise<void> {
   try {
-    const response: Response = await fetch(
-      'http://localhost:3000/inventari_type/',
-      {
-        method: 'GET',
-        headers: headersList,
-      },
-    );
+    const response: Response = await fetch('http://localhost:8080/inventari_type/', {
+      method: 'GET',
+      headers: headersList,
+    });
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
     const data: InventariType[] = await response.json();
-    console.log(data);
+    console.log('GET response data:', data);
   } catch (error) {
     console.error('Error fetching inventari types:', error);
   }
 }
+
+async function createInventariType(): Promise<void> {
+  const bodyContent: string = JSON.stringify({
+    id: 1,
+    descripcion: 'Telefono',
+  });
+
+  try {
+    const response: Response = await fetch('http://localhost:8080/inventari_type', {
+      method: 'POST',
+      body: bodyContent,
+      headers: headersList,
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data: string = await response.text();
+    console.log('POST response data:', data);
+  } catch (error) {
+    console.error('Error creating inventari type:', error);
+  }
+}
+
+async function updateInventariType(): Promise<void> {
+  const bodyContent: string = JSON.stringify({
+    descripcion: 'Playstation',
+  });
+
+  try {
+    const response: Response = await fetch('http://localhost:8080/inventari_type/1', {
+      method: 'PUT',
+      body: bodyContent,
+      headers: headersList,
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data: string = await response.text();
+    console.log('PUT response data:', data);
+  } catch (error) {
+    console.error('Error updating inventari type:', error);
+  }
+}
+
 fetchInventariTypes();
+createInventariType();
+updateInventariType();
