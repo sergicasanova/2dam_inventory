3f45c7e9e6f48694a97dbebcb24e52a8c2a52958
David <davidmunteanonti@gmail.com>
2024-10-28 16:39:21 +0100

    añado seeds para Issues, modifico entity y spec para cambiar de string a date

--- src/data/inventory_issues.ts
+++ src/data/inventory_issues.ts
@@ -1,42 +1,25 @@
 export default [
   {
-    id_issue: 1,
     id_inventory: 1,
-    created_at: '18/09/2024',
+    created_at: '2024-09-18',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     id_status: 1,
     id_user: 1,
     id_tecnic: 3,
-    last_updated: '18/09/2024',
+    last_updated: '2024-09-18',
     notes:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
-
   {
-    id_issue: 2,
     id_inventory: 2,
-    created_at: '18/09/2024',
+    created_at: '2024-09-18',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     id_status: 3,
     id_user: 3,
     id_tecnic: 9,
-    last_updated: '18/09/2024',
-    notes:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  },
-
-  {
-    id_issue: 3,
-    id_inventory: 5,
-    created_at: '18/09/2024',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    id_status: 4,
-    id_user: 4,
-    id_tecnic: 8,
-    last_updated: '18/09/2024',
+    last_updated: '2024-09-18',
     notes:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
