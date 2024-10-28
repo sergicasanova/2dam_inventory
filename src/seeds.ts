e811bfee3cfb22ff86db04a574a883507eac3d7a
David <davidmunteanonti@gmail.com>
2024-10-28 16:31:07 +0100

    añado seeds para Issues, modifico entity y spec para cambiar de string a date

--- src/seeds.ts
+++ src/seeds.ts
@@ -1,33 +1,47 @@
-import "reflect-metadata";
-import { DataSource, DataSourceOptions } from "typeorm";
-import { runSeeders, SeederOptions } from "typeorm-extension";
+import 'reflect-metadata';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { runSeeders, SeederOptions } from 'typeorm-extension';
 import { Inventari } from './inventari/inventari.entity';
-import { InventariFactory } from "./db/seeding/factories/inventari.factory"; 
-import { InevntariSeeder } from "./db/seeding/seeds/inventariSeed"; 
-import { Inventari_type } from "./inventari_type/inventari_type.entity";
+import { InventariSeeder } from './db/seeding/seeds/inventariSeed';
+import { Inventari_type } from './inventari_type/inventari_type.entity';
 import { Issue } from './issues/issues.entity';
 import { IssueConversationEntity } from './issues_conversation/issues_conversation.entity';
 import { User } from './users/users.entity';
 import { Status } from './status/status.entity';
 import { Classroom } from './classroom/classroom.entity';
+import { IssueSeeder } from './db/seeding/seeds/issues.seeder';
+import { UserSeeder } from './db/seeding/seeds/users.seeder';
+import { Inventary_typeSeeder } from './db/seeding/seeds/inventari_type.seeds';
+import { config } from 'dotenv';
+config();
 
 const options: DataSourceOptions & SeederOptions = {
-  type: "mariadb", 
-  host: "database", 
-  port: 3306, 
-  username: process.env.MYSQL_USER, 
+  type: 'mariadb',
+  host: 'database',
+  port: 3306,
+  username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 
-  entities: [Inventari, Inventari_type, Issue, IssueConversationEntity, User, Status, Classroom], 
-  factories: [InventariFactory], 
-  seeds: [InevntariSeeder], 
+  entities: [
+    Inventari,
+    Inventari_type,
+    Issue,
+    IssueConversationEntity,
+    User,
+    Status,
+    Classroom,
+  ],
+  seeds: [InventariSeeder, Inventary_typeSeeder, UserSeeder, IssueSeeder],
 };
 
 const dataSource = new DataSource(options);
 
-dataSource.initialize().then(async () => {
-  await dataSource.synchronize(true); 
-  await runSeeders(dataSource); 
-  process.exit(); 
-}).catch(error => console.log("Error initializing data source", error));
+dataSource
+  .initialize()
+  .then(async () => {
+    await dataSource.synchronize(true);
+    await runSeeders(dataSource);
+    process.exit();
+  })
+  .catch((error) => console.log('Error initializing data source', error));
