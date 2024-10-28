bddf6182e48f1d252cd54251e86c429751bf7644
Alonso <alonsoferrborr@gmail.com>
2024-10-28 15:26:49 +0100

    inventory_type seed

---
+++ src/db/seeding/seeds/inventari_type.seeds.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+import { Seeder } from "typeorm-extension";
+import { Inventari_type } from "../../../inventari_type/inventari_type.entity";
+import inventariTypeData from "../../../data/inventory_type"; 
+
+export class MainSeeder implements Seeder {
+  public async run(dataSource: DataSource): Promise<void> {
+    const inventariTypeRepository = dataSource.getRepository(Inventari_type);
+
+    await inventariTypeRepository.save(inventariTypeData);
+
+    console.log("Datos de Inventari_type insertados desde inventari_type.data.ts");
+  }
+}
+2
