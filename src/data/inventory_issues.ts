16890f802130102e727f6ac7bddd2b0f441871cd
davidmunt <163463306+davidmunt@users.noreply.github.com>
2024-10-30 13:05:11 +0100

    Update inventory_issues.ts

--- src/data/inventory_issues.ts
+++ src/data/inventory_issues.ts
@@ -1,25 +1,21 @@
 export default [
   {
     id_inventory: 1,
-    created_at: '2024-09-18',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     id_status: 1,
     id_user: 1,
     id_tecnic: 3,
-    last_updated: '2024-09-18',
     notes:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
     id_inventory: 2,
-    created_at: '2024-09-18',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     id_status: 3,
     id_user: 3,
     id_tecnic: 9,
-    last_updated: '2024-09-18',
     notes:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
