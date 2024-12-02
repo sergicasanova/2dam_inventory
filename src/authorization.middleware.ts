ef3f0a91bf8658c482d93ef3a5b8d8c4b3b5b3d6
chverma <chverma@users.noreply.github.com>
2024-12-02 16:38:09 +0100

    ENABLE_TOKEN_VALIDATION to enable o disable tokens

--- src/authorization.middleware.ts
+++ src/authorization.middleware.ts
@@ -8,17 +8,19 @@ export class AuthorizationMiddleware implements NestMiddleware {
   constructor(private readonly authService: AuthService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers['authorization'];
+    if (process.env.ENABLE_TOKEN_VALIDATION === 'true') {
+      const token = req.headers['authorization'];
 
-    if (!token) {
-      throw new UnauthorizedException('Token no encontrado');
-    }
+      if (!token) {
+        throw new UnauthorizedException('Token no encontrado');
+      }
 
-    const tokenSplit = token!.split(' ')[1];
+      const tokenSplit = token!.split(' ')[1];
 
-    const isValid = await this.authService.validateToken(tokenSplit);
-    if (!isValid) {
-      throw new UnauthorizedException('Token inválido o expirado');
+      const isValid = await this.authService.validateToken(tokenSplit);
+      if (!isValid) {
+        throw new UnauthorizedException('Token inválido o expirado');
+      }
     }
     next();
   }
