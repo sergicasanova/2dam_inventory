7f30e86f3d8f89ad48eb181e5876f636c270b42d
Alonso <alonsoferrborr@gmail.com>
2024-12-04 17:05:00 +0100

    Assignacion automatica de tecnico con menos incidencias

--- src/issues/issues.module.ts
+++ src/issues/issues.module.ts
@@ -4,9 +4,10 @@ import { IssuesService } from './issues.service';
 import { UtilsModule } from '../utils/utils.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Issue } from './issues.entity';
+import { User } from 'src/users/users.entity';
 
 @Module({
-  imports: [UtilsModule, TypeOrmModule.forFeature([Issue])],
+  imports: [UtilsModule, TypeOrmModule.forFeature([Issue, User ])],
   controllers: [IssuesController],
   providers: [IssuesService],
 })
--- src/issues/issues.service.ts
+++ src/issues/issues.service.ts
@@ -3,12 +3,15 @@ import { UtilsService } from '../utils/utils.service';
 import { Issue } from './issues.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { User } from 'src/users/users.entity';
 
 @Injectable()
 export class IssuesService {
   constructor(
     private readonly UtilsService: UtilsService,
     @InjectRepository(Issue) private issueRepository: Repository<Issue>,
+    @InjectRepository(User)
+  private userRepository: Repository<User>,
   ) {}
 
   async getAllIssues(xml?: string): Promise<Issue[] | string> {
@@ -26,11 +29,31 @@ export class IssuesService {
     }
   }
 
-  async createIssue(Issue: any): Promise<Issue[]> {
-    const newIssue = this.issueRepository.create(Issue);
-    return this.issueRepository.save(newIssue);
-  }
+  async createIssue(issueData: any): Promise<Issue> {
+    const technicianWithLeastIssues: User | null = await this.userRepository
+      .createQueryBuilder('user')
+      .leftJoin('user.assignedIssues', 'issue')
+      .where('user.role = :role', { role: 2 })
+      .groupBy('user.id_user')
+      .orderBy('COUNT(issue.id_issue)', 'ASC')
+      .getOne();
+  
+    if (!technicianWithLeastIssues) {
+      throw new Error('No se encontró un técnico disponible');
+    }
+  
+    const newIssue = this.issueRepository.create({
+      ...issueData, 
+      technician: technicianWithLeastIssues,
+    });
+    const savedIssue = await this.issueRepository.save(newIssue);
+    if (Array.isArray(savedIssue)) {
+      throw new Error("save() returned an array, expected an object.");
+    }
+    return savedIssue;
 
+}
+  
   async getIssue(id: number, xml?: string): Promise<Issue | string | null> {
     const issue = await this.issueRepository.findOneBy({ id_issue: id });
     if (issue != null) {
